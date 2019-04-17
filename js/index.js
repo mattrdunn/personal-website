ad121e6566e0e2d7a67b1281e4444b4fe67907d8
mattrdunn <mardunn@ucsc.edu>
2019-04-17 14:51:10 -0700

    Uploading most recent work

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function selectNav(clicked) {
+    let elems = document.getElementById("nav-bar").children;
+    for(let i=0; i < elems.length; i++) {
+        if(elems[i].className == "item active nav-item")
+            elems[i].className = "item nav-item";
+    }
+    clicked = document.getElementById(clicked);
+    clicked.className = "item active nav-item";
+}
